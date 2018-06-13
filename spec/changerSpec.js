a2a55bfee0d1eae5d94a6c96519b74a2e5ac6070
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-06-13 17:32:53 +0100

    coins are properly counted

--- spec/changerSpec.js
+++ spec/changerSpec.js
@@ -7,8 +7,37 @@ describe("coinChanger", function(){
   });
 
   describe("calculateCoins", function(){
-    it("should return 1", function(){
-      // expect(testChanger.returnOne()).toEqual(1);
+    it("should return an object with correct amounts of each coin", function(){
+      var actual = testChanger.calculateCoins(25);
+      var expected = {
+        quarters: 1,
+        dimes: 0,
+        nickels: 0,
+        pennies: 0
+      };
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return an object with correct amounts of each coin", function(){
+      var actual = testChanger.calculateCoins(1);
+      var expected = {
+        quarters: 0,
+        dimes: 0,
+        nickels: 0,
+        pennies: 1
+      };
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return an object with correct amounts of each coin", function(){
+      var actual = testChanger.calculateCoins(19);
+      var expected = {
+        quarters: 0,
+        dimes: 1,
+        nickels: 1,
+        pennies: 4
+      };
+      expect(actual).toEqual(expected);
     });
   });
 
@@ -22,7 +51,49 @@ describe("coinChanger", function(){
     });
 
     it("should return 3 from 85 cents", function(){
-      expect(testChanger.numberOfQuarters(25)).toEqual(1);
+      expect(testChanger.numberOfQuarters(85)).toEqual(3);
+    });
+  });
+
+  describe("numberOfDimes", function(){
+    it("should return 1 from 10 cents", function(){
+      expect(testChanger.numberOfDimes(10)).toEqual(1);
+    });
+
+    it("should return 5 from 50 cents", function(){
+      expect(testChanger.numberOfDimes(50)).toEqual(5);
+    });
+
+    it("should return 8 from 85 cents", function(){
+      expect(testChanger.numberOfDimes(85)).toEqual(8);
+    });
+  });
+
+  describe("numberOfNickels", function(){
+    it("should return 1 from 5 cents", function(){
+      expect(testChanger.numberOfNickels(5)).toEqual(1);
+    });
+
+    it("should return 5 from 50 cents", function(){
+      expect(testChanger.numberOfNickels(50)).toEqual(10);
+    });
+
+    it("should return 17 from 85 cents", function(){
+      expect(testChanger.numberOfNickels(85)).toEqual(17);
+    });
+  });
+
+  describe("numberOfPennies", function(){
+    it("should return 1 from 1 cent", function(){
+      expect(testChanger.numberOfPennies(1)).toEqual(1);
+    });
+
+    it("should return 50 from 50 cents", function(){
+      expect(testChanger.numberOfPennies(50)).toEqual(50);
+    });
+
+    it("should return 85 from 85 cents", function(){
+      expect(testChanger.numberOfPennies(85)).toEqual(85);
     });
   });
 
--- src/changer.js
+++ src/changer.js
@@ -3,8 +3,38 @@ var exports = module.exports = {};
 exports.Changer = function(){
 
   // Should take number of cents and return optimal amount of coins
-  // as a string like "3 quarters, 1 dimes, 1 nickel, 3 pennies"
-  this.numberOfQuarters = function(cents){
-    return cents / 25;
+  // as a string like "3 quarters, 1 dime, 1 nickel, 3 pennies"
+
+  this.calculateCoins = function(amount) {
+    this.remainder = amount;
+    var output = {
+      quarters: this.numberOfQuarters(this.remainder),
+      dimes: this.numberOfDimes(this.remainder),
+      nickels: this.numberOfNickels(this.remainder),
+      pennies: this.numberOfPennies(this.remainder),
+    };
+    return output;
+  };
+
+  this.numberOfQuarters = function(cents) {
+    var quarters = Math.floor(cents / 25);
+    this.remainder -= quarters * 25;
+    return quarters;
+  };
+
+  this.numberOfDimes = function(cents) {
+    var dimes = Math.floor(cents / 10);
+    this.remainder -= dimes * 10;
+    return dimes;
+  };
+
+  this.numberOfNickels = function(cents) {
+    var nickels = Math.floor(cents / 5);
+    this.remainder -= nickels * 5;
+    return nickels;
+  };
+
+  this.numberOfPennies = function(cents) {
+    return cents;
   };
 };
