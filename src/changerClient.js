2d15126d54ba40dd514a4ec394364d1530e3e577
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-06-25 17:16:42 +0100

    currency can be changed on the fly

--- src/changerClient.js
+++ src/changerClient.js
@@ -1,8 +1,30 @@
 var usd = {
-  quarters: 25,
-  dimes: 10,
-  nickels: 5,
-  pennies: 1
+  "Quarters": 25,
+  "Dimes": 10,
+  "Nickels": 5,
+  "Pennies": 1
+};
+
+var gbp = {
+  "2 Pounds": 200,
+  "1 Pound": 100,
+  "50p": 50,
+  "20p": 20,
+  "10p": 10,
+  "5p": 5,
+  "2p": 2,
+  "1p": 1
+};
+
+var euro = {
+  "2 Euros": 200,
+  "1 Euro": 100,
+  "50 Cents": 50,
+  "20 Cents": 20,
+  "10 Cents": 10,
+  "5 Cents": 5,
+  "2 Cents": 2,
+  "1 Cent": 1
 };
 
 function Changer(currency){
@@ -25,17 +47,29 @@ function Changer(currency){
   };
 }
 
+
 var changer = new Changer(usd);
 
 var changerApp = new Vue({
   el: '#changerApp',
   data: {
+    changer: changer,
     cents: 0,
-    coins: changer.calculateCoins(0)
+    coins: this.changer.calculateCoins(0),
+    currencies: [
+      { name: "USD", currency: usd },
+      { name: "GBP", currency: gbp },
+      { name: "EUR", currency: euro }
+    ],
+    currentCurrency: usd
   },
   methods: {
     calculator: function(){
-      this.coins = changer.calculateCoins(this.cents);
+      this.coins = this.changer.calculateCoins(this.cents);
+    },
+    updateCurrency: function(currency){
+      this.changer = new Changer(currency);
+      this.calculator();
     }
   }
 });
